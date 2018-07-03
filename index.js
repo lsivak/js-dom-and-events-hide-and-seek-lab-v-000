3eb8cc4359d2aa2bc2acaa2f34a2cd18f529c900
Lara Sanders <larasanders@gmail.com>
2018-07-03 20:58:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function getFirstSelector(selector) {
   const selection = document.querySelector(selector)
   return selection
 }
+
+function nestedTarget() {
+  const tar = document.querySelector(#nested)
+  return tar
+}
